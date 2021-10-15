67f0e3116769042025158dcb95fba629769a4c49
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-15 20:39:01 +0800

    servlet module atguigu路径下的新写的HelloServlet

--- 06_servlet/src/main/java/com/atguigu/HelloServlet.java
+++ 06_servlet/src/main/java/com/atguigu/HelloServlet.java
@@ -1,18 +1,9 @@
-package com.atguigu.servlet;
+package com.atguigu;
 
 import javax.servlet.*;
 import java.io.IOException;
 
-
-/**
- * hello servlet
- *
- * @author DELL
- * @date 2021/10/14
- */
 public class HelloServlet implements Servlet {
-
-
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
 
@@ -23,18 +14,9 @@ public class HelloServlet implements Servlet {
         return null;
     }
 
-    /**
-     * service()方法是专门用来处理客户端的请求的, 并处理后给出响应
-     * 简单来说就是, 只要前端有请求, 访问了Servlet -> 那么就执行HelloServlet的service方法
-     *
-     * @param servletRequest 请求
-     * @param servletResponse 回应
-     * @throws ServletException 异常显示
-     * @throws IOException IO异常
-     */
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("HelloServlet 被访问了");
+        System.out.println("HelloServlet中的service被执行了");
     }
 
     @Override
@@ -47,9 +29,3 @@ public class HelloServlet implements Servlet {
 
     }
 }
-
-
-
-
-
-

ae6328112af48ea555cd073dde373ef8bebd8a1d
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-15 21:27:57 +0800

    servlet module 集成HttpServlet类实现servlet程序 HelloServlet3 1.0

---
+++ 06_servlet/src/main/java/com/atguigu/HelloServlet3.java
@@ -0,0 +1,20 @@
+package com.atguigu;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet3 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("这里是继承了HttpServlet这个类, 因为它是已经封装好了一部分, 加快我们的开发");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("这里是继承了HttpServlet这个类, 因为它是已经封装好了一部分, 加快我们的开发");
+    }
+}

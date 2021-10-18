1532f7638dfe430d7c7c00e8195b1f5d1895d241
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-18 14:32:28 +0800

    servlet module 测试web工程的域对象存储数据的作用 ContextServlet2 ContextServlet3 1.0

---
+++ 06_servlet/src/main/java/com/atguigu/servlet/ContextServlet2.java
@@ -0,0 +1,23 @@
+package com.atguigu.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ContextServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // 直接获取servletContext对象
+        ServletContext servletContext = getServletContext();
+
+        // 未设置值的时候, 返回null
+        System.out.println("设置值之前: ContextServlet1 中获取域值数据key1的值 -> " + servletContext.getAttribute("key1"));
+        servletContext.setAttribute("key1", "value1");
+
+        System.out.println("ContextServlet1 中获取域值数据key1的值 -> " + servletContext.getAttribute("key1"));
+        System.out.println("ContextServlet1 中获取域值数据key1的值 -> " + servletContext.getAttribute("key1"));
+        System.out.println("ContextServlet1 中获取域值数据key1的值 -> " + servletContext.getAttribute("key1"));
+    }
+
+}
---
+++ 06_servlet/src/main/java/com/atguigu/servlet/ContextServlet3.java
@@ -0,0 +1,27 @@
+package com.atguigu.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ContextServlet3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /*
+            因为web工程的context-param配置只有一份, 一个web工程一份.
+            所以, 当我们运行了ContextServlet2程序后, context-param就有了初始值, 那么在ContextServlet3中也能访问到
+         */
+        ServletContext servletContext = getServletContext();
+        System.out.println("在ContextServlet3中获取域数据 key1 参数的值 -> " + servletContext.getAttribute("key1"));
+        System.out.println("在ContextServlet3中获取域数据 key1 参数的值 -> " + servletContext.getAttribute("key1"));
+
+
+        /*
+            如果一开始就访问contextServlet3网址, key1的值为null. 但是当访问过contextServlet2之后再访问contextServlet3时,
+                key1就会有值. 因为在contextServlet2中被赋值过了.
+         */
+    }
+
+
+}

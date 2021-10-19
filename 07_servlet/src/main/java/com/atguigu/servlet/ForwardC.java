2bf9c51c3490d6a19536232f65d49f6bb21ac6ae
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-19 23:51:30 +0800

    07servlet module 配合c页面的使用的servlet程序 ForwardC 1.0 (这里不太懂逻辑, 再想一下)

---
+++ 07_servlet/src/main/java/com/atguigu/servlet/ForwardC.java
@@ -0,0 +1,39 @@
+package com.atguigu.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+
+
+/**
+ * forwardc
+ *
+ * @author DELL
+ * @date 2021/10/19
+ */
+public class ForwardC extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        /*
+            经过ForwardC程序跳转到界面c
+         */
+
+        // 获取路由地址 -> 记住是以斜杠/ 打头
+        /*
+            这个requestDispatcher.forward方法, 不光能够将请求转发到目的地址就是getRequestDispatcher中的地址, 还能跳转
+         */
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/test/dispatcher/c.html");
+        // forward
+        System.out.println(requestDispatcher);
+        requestDispatcher.forward(request, response);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

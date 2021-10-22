0c5aa48f3a2e3e48460d46951b2620cd23794ad5
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-22 16:43:49 +0800

    bookStore 数据库中t_user表对应的JavaBean类

---
+++ bookSstore/src/main/java/com/atguigu/pojo/user.java
@@ -0,0 +1,142 @@
+package com.atguigu.pojo;
+
+
+
+/**
+ * 用户
+ *
+ * @author DELL
+ * @date 2021/10/22
+ */
+public class user {
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 电子邮件
+     */
+    private String email;
+
+    /**
+     * 用户
+     */
+    public user() {
+    }
+
+    /**
+     * 用户
+     *
+     * @param id       id
+     * @param username 用户名
+     * @param password 密码
+     * @param email    电子邮件
+     */
+    public user(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    /**
+     * 得到id
+     *
+     * @return {@code Integer}
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * 组id
+     *
+     * @param id id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    /**
+     * 获得用户名
+     *
+     * @return {@code String}
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置用户名
+     *
+     * @param username 用户名
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 得到密码
+     *
+     * @return {@code String}
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * 设置密码
+     *
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * 获得电子邮件
+     *
+     * @return {@code String}
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置电子邮件
+     *
+     * @param email 电子邮件
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    /**
+     * 字符串
+     *
+     * @return {@code String}
+     */
+    @Override
+    public String toString() {
+        return "user{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

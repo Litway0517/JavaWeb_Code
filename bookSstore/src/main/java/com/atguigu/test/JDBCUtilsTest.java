8219c86cacb446f6733918d908fcd6164c49b792
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-23 23:02:17 +0800

    bookStore utils目录 编写常用工具类JDBCUtils 1.1重新替换了文件

---
+++ bookSstore/src/main/java/com/atguigu/test/JDBCUtilsTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.test;
+
+
+import com.atguigu.utils.JDBCUtils;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+public class JDBCUtilsTest {
+
+    @Test
+    public void testJDBCUtils() throws Exception {
+
+        Connection conn = JDBCUtils.getConnection();
+        System.out.println(conn);
+    }
+
+}
---
+++ bookSstore/src/main/java/com/atguigu/utils/JDBCUtils.java
@@ -0,0 +1,81 @@
+package com.atguigu.utils;
+
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * jdbcutils
+ *
+ * @author DELL
+ * @date 2021/10/10
+ */
+
+
+/*
+    JDBC工具类, 可以直接使用这里面的方法获取连接, 而不需要再重写代码
+ */
+public class JDBCUtils {
+
+    /*
+        因为下面的静态方法, static初始化方法, 需要用到, dataSource这个变量, 而静态方法只能调用静态的对象.
+        因此, dataSource需要用static修饰
+
+     */
+    // 定义一个类的静态变量
+    private static DataSource dataSource;
+
+    /*
+        static代码块, 是当类加载完成的时候, 仅仅执行一次, 这样就只会有一个dataSource, 也就是只有一个数据池
+        static代码块执行的目的就是为了创建一个数据池, 并赋值给外面的静态dataSource变量
+     */
+    static {
+        try {
+            // 创建prop对象
+            Properties prop = new Properties();
+            // 读取配置文件
+            InputStream fr = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
+            prop.load(fr);
+
+            dataSource = DruidDataSourceFactory.createDataSource(prop);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+        每次调用这个方法, 仅仅是从数据库连接池中拿一个连接使用.
+        而不是像之前的方法, 每次都创建一个数据池, 再从数据池中拿一个连接(这种方式更慢)
+     */
+    public static Connection getConnection() throws Exception {
+        // 获取一个连接
+        return dataSource.getConnection();
+    }
+
+    /*
+        因为, 每次使用完毕之后, 都需要关闭掉数据库的连接. 所以也封装成一个方法
+     */
+    public static void close(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+}
+
+
+
+
+

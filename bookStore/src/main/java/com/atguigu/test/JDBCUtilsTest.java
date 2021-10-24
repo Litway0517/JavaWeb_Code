373d3e640785fe9fd169578d7993186207b3bd0e
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-24 15:28:16 +0800

    bookStore 更新JDBCUtilsTest测试

--- bookStore/src/main/java/com/atguigu/test/JDBCUtilsTest.java
+++ bookStore/src/main/java/com/atguigu/test/JDBCUtilsTest.java
@@ -15,6 +15,20 @@ public class JDBCUtilsTest {
         System.out.println(conn);
 
         JDBCUtils.close(conn);
+
+        for (int i = 0; i < 50; i++) {
+            Connection connection = JDBCUtils.getConnection();
+            System.out.println("第" + i  + "个连接 -> " + connection);
+
+            /*
+                使用完之后不关闭, 那么这个链接会被一直占用, 因此要记住需要关闭连接
+                (其实, 一旦加了close(), 由于释放速度很快, 中间的间隙很小, 50次获取的链接是同一个emm)
+             */
+
+
+            JDBCUtils.close(connection);
+
+        }
     }
 
 }

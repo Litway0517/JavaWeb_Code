4566d14b6c18528de74785d60585ba2703376a40
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-25 10:42:33 +0800

    bookStore 实现UserBean对象的Web层服务 1.0

--- bookStore/src/main/java/com/atguigu/web/servlet/RegisterServlet.java
+++ bookStore/src/main/java/com/atguigu/web/servlet/RegisterServlet.java
@@ -1,5 +1,8 @@
 package com.atguigu.web.servlet;
 
+import com.atguigu.pojo.User;
+import com.atguigu.service.impl.UserServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +17,9 @@ import java.io.IOException;
  * @date 2021/10/24
  */
 public class RegisterServlet extends HttpServlet {
+
+    // 实例化Service层 -> 供下面的web层调用
+    private UserServiceImpl userService = new UserServiceImpl();
 
 
     /**
@@ -43,9 +49,33 @@ public class RegisterServlet extends HttpServlet {
         // 设置字符集
         req.setCharacterEncoding("UTF-8");
 
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String repwd = req.getParameter("repwd");
+        String email = req.getParameter("email");
+        String code = req.getParameter("code");
 
 
+        // 目前还没有服务器生成验证码 -> 暂时将验证码写成固定的abcde, 且不区分大小写
+        if (code.equalsIgnoreCase("abcde")) {
+            // 检查用户名是否可用 -> Web不能直接调用DAO层, 而是使用Service层完成的, 因此需要在 '方法体外' 实例化UserService层
+            if (userService.existsUsername(username)) {
+                // 返回true表示不能使用该用户名注册 -> 提醒 + 跳转到注册界面
+                System.out.println("用户名 [" + username + "] 已存在! ");
+                // 跳到注册界面
+                req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
+            } else {
+                // 用户名不存在表中 -> 可用情况 -> 调用UserService表存到表中 -> 在跳转到注册成功界面
+                userService.registerUser(new User(null, username, password, email));
+                req.getRequestDispatcher("/pages/user/regist_success.html").forward(req, resp);
+            }
+        } else {
+            // 验证码错误 -> 跳转到注册界面
+            System.out.println("验证码 [" + code + "] 错误");
+            // 这里面也验证了, getRequestDispatcher方法是具有跳转页面的功能的
+            req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
 
+        }
 
 
         super.doPost(req, resp);

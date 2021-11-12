97c7ac96a776077ef6c9ac979d4ceb890f77736b
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-12 23:36:32 +0800

    bookStore 优化UserServlet代码

--- bookStore/src/main/java/com/atguigu/web/servlet/user/UserServlet.java
+++ bookStore/src/main/java/com/atguigu/web/servlet/user/UserServlet.java
@@ -5,32 +5,15 @@ import com.atguigu.service.UserService;
 import com.atguigu.service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.Method;
 
-public class UserServlet extends HttpServlet {
+public class UserServlet extends BaseServlet {
 
     UserService userService = new UserServiceImpl();
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 前端的两个表单的input的name值都叫做action就行了, 所以传过来那个额都能够接收到
-        String action = req.getParameter("action");
 
-        try {
-            // 获取反射对象
-            Method method = this.getClass().getDeclaredMethod(action, HttpServletRequest.class, HttpServletResponse.class);
-            // 调用方法
-            method.invoke(this, req, resp);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
 
 
     protected void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

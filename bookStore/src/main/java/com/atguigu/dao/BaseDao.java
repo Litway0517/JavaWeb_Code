45804984433f696f7a1929b86898de29a8197797
Mingw0717-Dell <xmw0517@gmail.com>
2021-11-14 15:37:15 +0800

    bookStore 更新一些变量名称, 更容易理解Bean

--- bookStore/src/main/java/com/atguigu/dao/BaseDao.java
+++ bookStore/src/main/java/com/atguigu/dao/BaseDao.java
@@ -52,19 +52,19 @@ public abstract class BaseDao {
 
     /**
      * 查询返回一个JavaBean对象或者返回一条记录的sql语句
-     * @param type 是查询数据库的表时, 返回的数据对象时什么类型. 其实就是pojo目录下面的JavaBean对象.
+     * @param bean 是查询数据库的表时, 返回的数据对象时什么类型. 其实就是pojo目录下面的JavaBean对象.
      *                  可能是一个user对象.
      * @param sql 待执行的sql语句
      * @param args 执行sql语句时, 需要有参数装配, 这个args就是该种作用. 是一个可变参数, 因为不知道需要具体传入多少个参数
      * @param <T> T就是返回的泛型, 和type一致的. 就是用来描述type
      * @return 返回的结果集合, 针对本方法是一个对象. 返回空表示查询失败
      */
-    public <T> T queryForOne(Class<T> type, String sql, Object ...args) {
+    public <T> T queryForOne(Class<T> bean, String sql, Object ...args) {
         /*
             Class<T> 就是获取T对象的Class对象, 这个Class就是反射里面那个Class
          */
         Connection conn = JDBCUtils.getConnection();
-        BeanHandler<T> beanHandler = new BeanHandler<>(type);
+        BeanHandler<T> beanHandler = new BeanHandler<>(bean);
 
         try {
             return queryRunner.query(conn, sql,beanHandler, args);
@@ -79,19 +79,19 @@ public abstract class BaseDao {
 
     /**
      * 查询表中的多条数据, 返回多个JavaBean对象, 并将这些对象封装到一个List集合中
-     * @param type 返回对象的类型
+     * @param bean 返回对象的类型
      * @param sql 待执行的sql语句
      * @param args sql语句中的参数
      * @param <T> 返回的类型
      * @return 若返回null, 表示查询失败. 非空则表示查询成功
      */
-    public <T> List<T> queryForList(Class<T> type, String sql, Object ...args) {
+    public <T> List<T> queryForList(Class<T> bean, String sql, Object ...args) {
         /*
             public 这是一个泛型方法, <T>紧跟在public后面就说明了这点. T作为泛型. 方法返回的是List列表, 列表中存储的元素的类型就是泛型T
 
          */
         Connection conn = JDBCUtils.getConnection();
-        BeanListHandler<T> beanListHandler = new BeanListHandler<T>(type);
+        BeanListHandler<T> beanListHandler = new BeanListHandler<T>(bean);
 
         try {
             return queryRunner.query(conn, sql,beanListHandler, args);

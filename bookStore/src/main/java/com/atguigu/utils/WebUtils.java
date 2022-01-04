a075bd4468ff746c6e11911d0a18e728dc518ff3
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-04 15:30:56 +0800

    bookStore 注释更新

--- bookStore/src/main/java/com/atguigu/utils/WebUtils.java
+++ bookStore/src/main/java/com/atguigu/utils/WebUtils.java
@@ -16,7 +16,7 @@ public class WebUtils {
     /**
      * 将参数赋值给JavaBean对象
      *
-     *  这个方法得参数是一个Map value和一个Object bean对象. 并不是一个HttpServlet request和一个Object bean对象
+     *  copyParamsToBean(Map value, T bean)方法得参数是一个Map value和一个Object bean对象. 并不是一个HttpServlet request和一个Object bean对象
      *      理由是, 在JavaEE的三层架构中, DAO层 == Service层 == Web层 并不能使用HttpServlet类型的对象.
      *      简而言之, 现在就是为了获取Map中的参数传给Bean对象, 但是这个Map不一定都来自HttpServlet中的request中的Map啊?
      *      还可能来自其他地方, 所以改成Map类型. Map类型是包括HttpServlet中的request.getParameterMap()类型的.

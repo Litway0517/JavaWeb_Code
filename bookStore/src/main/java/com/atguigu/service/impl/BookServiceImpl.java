a8584dba8be529b798d9986da5d0fafbe71bdd5b
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-09 22:02:03 +0800

    bookStore 将这段代码放到Page类的setter方法中

--- bookStore/src/main/java/com/atguigu/service/impl/BookServiceImpl.java
+++ bookStore/src/main/java/com/atguigu/service/impl/BookServiceImpl.java
@@ -111,12 +111,12 @@ public class BookServiceImpl implements BookService {
             如果地址栏中输入的页码小于1, 则最后赋值1
             如果地址栏中输入的页码大于最大页码max, 则最后赋值最大页码max
          */
-        if (pageNo < 1) {
+        /*if (pageNo < 1) {
             pageNo = 1;
         }
         if (pageNo > pageTotal) {
             pageNo = pageTotal;
-        }
+        }*/
 
 
         // 5- 设置items的值, 就是查询当前页的数据

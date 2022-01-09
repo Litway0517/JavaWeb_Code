aaa780a09992d2d023ed7149f284dd2a13216e26
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-09 11:18:25 +0800

    bookStore 在BookService中增加分页page接口

--- bookStore/src/main/java/com/atguigu/service/BookService.java
+++ bookStore/src/main/java/com/atguigu/service/BookService.java
@@ -1,6 +1,7 @@
 package com.atguigu.service;
 
 import com.atguigu.pojo.Book;
+import com.atguigu.pojo.Page;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public interface BookService {
 
 
     /**
+     * 添加书
      * 添加的书
      *
      * @param book 书
@@ -21,6 +23,7 @@ public interface BookService {
     public void addBook(Book book);
 
     /**
+     * 更新书
      * 更新的书
      *
      * @param book 书
@@ -38,16 +41,26 @@ public interface BookService {
      * 通过id查询书
      *
      * @param id id
-     * @return {@code Book}
+     * @return {@link Book}
      */
     public Book queryBookById(Integer id);
 
     /**
      * 查询图书
      *
-     * @return {@code List<Book>}
+     * @return {@link List}<{@link Book}>
      */
     public List<Book> queryBooks();
+
+
+    /**
+     * 页面
+     * 完成分页功能
+     * @param pageNo   页面编号
+     * @param pageSize 每一个页面的大小
+     * @return {@link Page} 返回的是分好的一页的数据, 包括页码, 总记录数, 总页数, 该页的四条数目数据等.
+     */
+    public Page<Book> page(int pageNo, int pageSize);
 
 
 

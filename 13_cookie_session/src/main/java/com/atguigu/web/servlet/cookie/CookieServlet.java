19292c7b2cc0df6f99d196f0e12407b4dab1a013
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-11 22:44:24 +0800

    bookStore 用于管理Cookie的类

---
+++ 13_cookie_session/src/main/java/com/atguigu/web/servlet/cookie/CookieServlet.java
@@ -0,0 +1,24 @@
+package com.atguigu.web.servlet.cookie;
+
+import com.atguigu.web.servlet.base.BaseServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CookieServlet extends BaseServlet {
+
+    /**
+     * 创建cookie
+     * @param req 请求
+     * @param resp 响应
+     * @throws ServletException servlet异常
+     * @throws IOException IO异常
+     */
+    protected void createCookie(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+
+}

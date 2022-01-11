6c3e989fb808b1e2f28d700696b4fee2b0a7d281
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-11 10:08:19 +0800

    bookStore

--- bookStore/src/main/java/com/atguigu/web/servlet/client/ClientBookServlet.java
+++ bookStore/src/main/java/com/atguigu/web/servlet/client/ClientBookServlet.java
@@ -31,8 +31,6 @@ public class ClientBookServlet extends BaseServlet {
      */
     protected void page(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        System.out.println("经过了前台的ClientBookServlet程序~");
-
         // 1- 获取请求的参数 pageNo 和 pageSize
         int pageNo = WebUtils.parseInt(req.getParameter("pageNo"), 1);
         int pageSize = WebUtils.parseInt(req.getParameter("pageSize"), Page.PAGE_SIZE);

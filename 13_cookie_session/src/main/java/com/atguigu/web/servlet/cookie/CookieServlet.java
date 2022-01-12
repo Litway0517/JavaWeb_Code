377bcf072b0bef7c7ab214866864b9684ee62e8e
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-12 09:56:13 +0800

    13_cookie_session 向浏览器添加Cookie: cookieServlet程序 resp.addCookie(Cookie c)

--- 13_cookie_session/src/main/java/com/atguigu/web/servlet/cookie/CookieServlet.java
+++ 13_cookie_session/src/main/java/com/atguigu/web/servlet/cookie/CookieServlet.java
@@ -3,6 +3,7 @@ package com.atguigu.web.servlet.cookie;
 import com.atguigu.web.servlet.base.BaseServlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,7 +19,25 @@ public class CookieServlet extends BaseServlet {
      * @throws IOException      IO异常
      */
     protected void createCookie(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 解决中文乱码
+        resp.setContentType("text/html; charset=UTF-8");
 
+        // 1- 创建cookie对象
+        Cookie cookie = new Cookie("cookie1", "testCookie1");
+
+        // 2- 服务器发送给客户端, 使用resp报文
+        resp.addCookie(cookie);
+        Cookie cookie1 = new Cookie("cookie2", "testCookie2");
+        resp.addCookie(cookie1);
+
+
+
+        resp.getWriter().write("cookie创建成功");
+
+
+
+
+        System.out.println("test");
 
 
     }

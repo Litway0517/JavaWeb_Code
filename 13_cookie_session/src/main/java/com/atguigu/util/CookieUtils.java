28d4d50d9859498836c60e34d36bd0dddb1e863f
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-12 11:05:33 +0800

    13_cookie_session

--- 13_cookie_session/src/main/java/com/atguigu/util/CookieUtils.java
+++ 13_cookie_session/src/main/java/com/atguigu/util/CookieUtils.java
@@ -2,8 +2,21 @@ package com.atguigu.util;
 
 import javax.servlet.http.Cookie;
 
+/**
+ * 饼干跑龙套
+ *
+ * @author DELL_
+ * @date 2022/01/12
+ */
 public class CookieUtils {
 
+    /**
+     * 发现饼干
+     *
+     * @param name    名字
+     * @param cookies 饼干
+     * @return {@link Cookie}
+     */
     public static Cookie findCookie(String name, Cookie[] cookies) {
         // 对传入的参数进行判断
         if (name == null || cookies == null || cookies.length == 0) {

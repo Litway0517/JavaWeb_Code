584d5bdce6b4c07032bf9ae38d47e50e414ea95a
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-13 10:38:32 +0800

    13_cookie_session 配置SessionServlet的xml路由信息

---
+++ 13_cookie_session/src/main/java/com/atguigu/web/servlet/session/SessionServlet.java
@@ -0,0 +1,21 @@
+package com.atguigu.web.servlet.session;
+
+import com.atguigu.web.servlet.base.BaseServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SessionServlet extends BaseServlet {
+
+
+    protected void session(HttpServletRequest req, HttpServletResponse response) {
+
+
+
+
+
+
+    }
+
+
+}

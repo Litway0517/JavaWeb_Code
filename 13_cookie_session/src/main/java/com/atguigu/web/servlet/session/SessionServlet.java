0e899b5c4fe707ff4fe493505c9648aac166100e
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-13 11:05:42 +0800

    13_cookie_session 后端测试session的创建与获取和判断是否为新创建的session true表示新创建

--- 13_cookie_session/src/main/java/com/atguigu/web/servlet/session/SessionServlet.java
+++ 13_cookie_session/src/main/java/com/atguigu/web/servlet/session/SessionServlet.java
@@ -4,15 +4,26 @@ import com.atguigu.web.servlet.base.BaseServlet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class SessionServlet extends BaseServlet {
 
 
-    protected void session(HttpServletRequest req, HttpServletResponse response) {
+    protected void createOrGetSession(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
 
+        // 创建和获取session对象
+        HttpSession session = request.getSession();
 
+        // 判断这个session是否为新 true -> 表示新创建的   false -> 表示旧的
+        boolean isNew = session.isNew();
 
+        // 获取session的唯一标识
+        String id = session.getId();
 
+        response.getWriter().write("得到的session, id是: <br/>" + id + "<br/>");
+        response.getWriter().write("这个session是否是新创建的: " + isNew);
 
 
     }

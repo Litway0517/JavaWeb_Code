b4dea242812795ae8f5b185fde2976ece9467a12
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-13 11:18:30 +0800

    13_cookie_session 向session域中存储数据 req.getSession().set/get Attribute(key, value)

--- 13_cookie_session/src/main/java/com/atguigu/web/servlet/session/SessionServlet.java
+++ 13_cookie_session/src/main/java/com/atguigu/web/servlet/session/SessionServlet.java
@@ -7,9 +7,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/**
+ * 会话servlet
+ *
+ * @author DELL_
+ * @date 2022/01/13
+ */
 public class SessionServlet extends BaseServlet {
 
 
+    /**
+     * 创建或获取会话
+     *
+     * @param request  请求
+     * @param response 响应
+     * @throws IOException IO异常
+     */
     protected void createOrGetSession(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html; charset=UTF-8");
 
@@ -25,6 +38,37 @@ public class SessionServlet extends BaseServlet {
         response.getWriter().write("得到的session, id是: <br/>" + id + "<br/>");
         response.getWriter().write("这个session是否是新创建的: " + isNew);
 
+
+    }
+
+
+    /**
+     * 设置属性
+     *
+     * @param req  要求事情
+     * @param resp 分别地
+     */
+    protected void setAttribute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // 设置session的属性. Session本身是一个域, 能够存储数据的, 别忘了
+        req.getSession().setAttribute("key1", "value1");
+        // 提示
+        resp.getWriter().write("已经向session中保存了数据");
+
+    }
+
+
+    /**
+     * 得到属性
+     *
+     * @param req  要求事情
+     * @param resp 分别地
+     */
+    protected void getAttribute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+
+        // 获取存储在Session域中的key1的数据
+        Object key1 = req.getSession().getAttribute("key1");
+        resp.getWriter().write("存储在session域中的key1的数据: " + key1);
 
     }
 

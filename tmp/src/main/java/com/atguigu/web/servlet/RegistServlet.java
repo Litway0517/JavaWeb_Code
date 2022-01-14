879e134fa004ab3188caf29106a33e0717bb3f05
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-14 11:47:33 +0800

    tmp

--- tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
+++ tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
@@ -1,6 +1,5 @@
 package com.atguigu.web.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +8,7 @@ import java.io.IOException;
 public class RegistServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.getParameter("username");
         System.out.println("保存到数据库");
 

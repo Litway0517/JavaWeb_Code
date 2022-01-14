d2cc08d55f56703a6c0b945144d9f275ca065fd7
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-14 11:40:08 +0800

    tmp 使用重定向 解决浏览器重复提交表单(F5刷新浏览器就会不断提交请求)的问题

--- tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
+++ tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
@@ -10,10 +10,17 @@ public class RegistServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
+        req.getParameter("username");
         System.out.println("保存到数据库");
 
         // 请求转发
-        req.getRequestDispatcher("/ok.jsp").forward(req, resp);
+//        req.getRequestDispatcher("/ok.jsp").forward(req, resp);
+
+        /*
+            因为使用请求转发时, 如果提交完毕之后, 客户端按F5刷新浏览器, 仍会提交最后一次的请求, 那么就不断的保存数据库.
+            所以, 这里使用重定向
+         */
+        resp.sendRedirect(req.getContextPath() + "/ok.jsp");
+
     }
 }

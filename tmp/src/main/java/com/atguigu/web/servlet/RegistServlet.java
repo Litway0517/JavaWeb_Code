669d9e83c13e5bb71bcc8aef7eda1ba8aab56a39
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-14 14:39:32 +0800

    tmp 谷歌的验证码第卅安防jar包创建的时候 验证码是放在session域中的

--- tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
+++ tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
@@ -1,16 +1,19 @@
 package com.atguigu.web.servlet;
 
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
+
 public class RegistServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        req.getParameter("username");
-        System.out.println("保存到数据库");
+        resp.setContentType("text/html; charset=UTF-8");
+        String username = req.getParameter("username");
 
         // 请求转发
 //        req.getRequestDispatcher("/ok.jsp").forward(req, resp);
@@ -27,14 +30,28 @@ public class RegistServlet extends HttpServlet {
                 由于网络原因, 用户以为请求失败, 用户重复点击了几次提交
                 实际上, 服务器已经插入了很多条记录.
          */
-        try {
+
+        /*try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }*/
+
+        // 上面的代码先注释掉, 使用谷歌验证码解决后两种表单重复提交的情况
+        // 获取session域中的验证码
+        String token = (String) req.getSession().getAttribute(KAPTCHA_SESSION_KEY);
+        // 获取之后立刻删除
+        req.getSession().removeAttribute(KAPTCHA_SESSION_KEY);
+
+        // 在进行逻辑操作
+        String code = req.getParameter("code");
+        if (token != null && token.equalsIgnoreCase(code)) {
+            System.out.println("保存到数据库中: " + username);
+            resp.sendRedirect(req.getContextPath() + "/ok.jsp");
+        } else {
+            resp.getWriter().write("请不要重复提交表单");
         }
 
-
-        resp.sendRedirect(req.getContextPath() + "/ok.jsp");
 
     }
 }

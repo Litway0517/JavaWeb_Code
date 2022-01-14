62701600a6dad37ff75581d031324c8f65b681b4
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-14 11:47:20 +0800

    tmp 第二种情况 当用户网络存在延迟 而以未请求失败时 多点击了几次提交

--- tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
+++ tmp/src/main/java/com/atguigu/web/servlet/RegistServlet.java
@@ -17,9 +17,24 @@ public class RegistServlet extends HttpServlet {
 //        req.getRequestDispatcher("/ok.jsp").forward(req, resp);
 
         /*
-            因为使用请求转发时, 如果提交完毕之后, 客户端按F5刷新浏览器, 仍会提交最后一次的请求, 那么就不断的保存数据库.
-            所以, 这里使用重定向
+            第一种情况, 提交请求后按F5刷新浏览器重新提交
+                因为使用请求转发时, 如果提交完毕之后, 客户端按F5刷新浏览器, 仍会提交最后一次的请求, 那么就不断的保存数据库.
+                所以, 这里使用重定向
+            解决办法: 使用重定向
          */
+
+        /*
+            第二种情况
+                由于网络原因, 用户以为请求失败, 用户重复点击了几次提交
+                实际上, 服务器已经插入了很多条记录.
+         */
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
         resp.sendRedirect(req.getContextPath() + "/ok.jsp");
 
     }

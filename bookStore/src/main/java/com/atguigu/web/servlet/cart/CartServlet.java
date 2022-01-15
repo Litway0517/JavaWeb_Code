01358756b52632d54401d4abf6a340c26cdb03bc
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-15 21:21:08 +0800

    bookStore 首页的加入购物车按钮 激活

--- bookStore/src/main/java/com/atguigu/web/servlet/cart/CartServlet.java
+++ bookStore/src/main/java/com/atguigu/web/servlet/cart/CartServlet.java
@@ -1,16 +1,46 @@
 package com.atguigu.web.servlet.cart;
 
+import com.atguigu.pojo.Book;
+import com.atguigu.pojo.Cart;
+import com.atguigu.pojo.CartItem;
+import com.atguigu.service.impl.BookServiceImpl;
+import com.atguigu.utils.WebUtils;
+import com.atguigu.web.servlet.base.BaseServlet;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
-public class CartServlet extends HttpServlet {
+public class CartServlet extends BaseServlet {
 
+    /**
+     * 增加商品项
+     * @param req 请求报文
+     * @param resp 响应报文
+     * @throws ServletException servlet异常
+     * @throws IOException IO异常
+     */
+    protected void addItem(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, InvocationTargetException, IllegalAccessException {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BookServiceImpl bookServiceImpl = new BookServiceImpl();
+
+        // 获取请求参数
+        int bookId = WebUtils.parseInt(req.getParameter("bookId"), 0);
+
+        // 根据客户端传过来的图书的id, 到数据库中查询相应的图书的具体信息
+        Book book = bookServiceImpl.queryBookById(bookId);
+
+        // 将查询到的信息赋值给CartItem实体类
+        CartItem cartItem = new CartItem(book.getId(), book.getBookName(), 1, book.getBookPrice(), book.getBookPrice());
+
+        // 调用加入购物车方法
+        Cart cart = new Cart();
+        cart.addItem(cartItem);
+
+        // 重定向回首页
+        resp.sendRedirect(req.getContextPath());
 
 
     }

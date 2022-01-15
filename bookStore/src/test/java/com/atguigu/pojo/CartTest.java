dab6fd816ad038ff919963b1a605401a7762ed35
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-15 10:51:37 +0800

    bookStore 测试Cart实体类的方法

---
+++ bookStore/src/test/java/com/atguigu/pojo/CartTest.java
@@ -0,0 +1,24 @@
+package com.atguigu.pojo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CartTest {
+
+    @Test
+    public void addItem() {
+    }
+
+    @Test
+    public void deleteItem() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void updateCount() {
+    }
+}

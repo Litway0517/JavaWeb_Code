dc4b37d572609b0667b730f0eb432e78ae3d13a7
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-15 20:09:38 +0800

    bookStore 四个方法测试(存在一个小的bug)

--- bookStore/src/test/java/com/atguigu/pojo/CartTest.java
+++ bookStore/src/test/java/com/atguigu/pojo/CartTest.java
@@ -6,27 +6,80 @@ import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
+/**
+ * 车测试
+ *
+ * @author DELL_
+ * @date 2022/01/15
+ */
 public class CartTest {
 
-    private final Cart cart = new Cart();
-
+    /**
+     * 添加物品
+     */
     @Test
     public void addItem() {
-        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 2, new BigDecimal("28.8"), new BigDecimal("28.8")));
-        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal("28.8"), new BigDecimal("28.8")));
-        cart.addItem(new CartItem(12, "计算机网络", 1, new BigDecimal("100"), new BigDecimal("100")));
+        Cart cart = new Cart();
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 2, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(20, "计算机网络", 1, new BigDecimal(100), new BigDecimal(100)));
         System.out.println(cart);
     }
 
+    /**
+     * 删除项目
+     */
     @Test
     public void deleteItem() {
+        Cart cart = new Cart();
+
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 2, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(20, "计算机网络", 1, new BigDecimal(100), new BigDecimal(100)));
+
+        // 删除一个商品
+        cart.deleteItem(10);
+
+        System.out.println(cart);
     }
 
+    /**
+     * 清晰
+     */
     @Test
     public void clear() {
+        Cart cart = new Cart();
+
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 2, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(20, "计算机网络", 1, new BigDecimal(100), new BigDecimal(100)));
+
+        // 清空购物车
+        cart.clear();
+
+        System.out.println(cart);
     }
 
+    /**
+     * 更新数
+     */
     @Test
     public void updateCount() {
+        /*
+            如果, 一上来的数量就超过1, 比如, 初始化的时候, '黑客帝国 矩阵'的数量为2, 但是单价和分总价格填的都是30时, 会引起bug
+         */
+        Cart cart = new Cart();
+
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(20, "计算机网络", 1, new BigDecimal(100), new BigDecimal(100)));
+
+        // 清空购物车
+        cart.clear();
+
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+
+        cart.updateCount(10, 10);
+        System.out.println(cart);
     }
 }

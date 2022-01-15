80315f546f9a4cc4c58e83ff6a7bdde5d138254e
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-15 12:14:01 +0800

    bookStore 测试Cart实体类的方法

--- bookStore/src/test/java/com/atguigu/pojo/CartTest.java
+++ bookStore/src/test/java/com/atguigu/pojo/CartTest.java
@@ -2,12 +2,20 @@ package com.atguigu.pojo;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 public class CartTest {
 
+    private final Cart cart = new Cart();
+
     @Test
     public void addItem() {
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 2, new BigDecimal("28.8"), new BigDecimal("28.8")));
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal("28.8"), new BigDecimal("28.8")));
+        cart.addItem(new CartItem(12, "计算机网络", 1, new BigDecimal("100"), new BigDecimal("100")));
+        System.out.println(cart);
     }
 
     @Test

4dc42d395edcc22143a74a1a1e4a06b746608988
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-15 10:50:20 +0800

    bookStore 增加业务方法: 直接在Cart实体类中增加四个方法 因为这次我们使用session来实现购物车 因此没有了数据库 那也就没有了DAO层 同样service层也没有

--- bookStore/src/main/java/com/atguigu/pojo/Cart.java
+++ bookStore/src/main/java/com/atguigu/pojo/Cart.java
@@ -1,15 +1,15 @@
 package com.atguigu.pojo;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * 车
  *
  * @author DELL_
  * @date 2022/01/14
- */
-public class Cart<T> {
+ */public class Cart {
 
     /**
      * 总菌数
@@ -22,7 +22,7 @@ public class Cart<T> {
     /**
      * 项目
      */
-    private List<T> items;
+    private Map<Integer, CartItem> items = new LinkedHashMap<Integer, CartItem>();
 
     /**
      * 车
@@ -37,7 +37,7 @@ public class Cart<T> {
      * @param totalPrice 总价格
      * @param items      项目
      */
-    public Cart(Integer totalCount, BigDecimal totalPrice, List<T> items) {
+    public Cart(Integer totalCount, BigDecimal totalPrice, Map<Integer, CartItem> items) {
         this.totalCount = totalCount;
         this.totalPrice = totalPrice;
         this.items = items;
@@ -82,9 +82,9 @@ public class Cart<T> {
     /**
      * 得到项目
      *
-     * @return {@link List}<{@link T}>
+     * @return {@link Map}<{@link Integer}, {@link CartItem}>
      */
-    public List<T> getItems() {
+    public Map<Integer, CartItem> getItems() {
         return items;
     }
 
@@ -93,7 +93,7 @@ public class Cart<T> {
      *
      * @param items 项目
      */
-    public void setItems(List<T> items) {
+    public void setItems(Map<Integer, CartItem> items) {
         this.items = items;
     }
 
@@ -110,4 +110,78 @@ public class Cart<T> {
                 ", items=" + items +
                 '}';
     }
+
+
+    // ===================================================================
+    /*
+        下面增加实现的方法:
+            - addItem(CartIem cartItem) {}
+            - deleteItem(Integer id) {}
+            - clearAll() {}
+            - updateCount(Integer id, Integer newCount) {}
+     */
+
+    /**
+     * 添加物品
+     *
+     * @param cartItem 车项目
+     */
+    public void addItem(CartItem cartItem){
+        /*
+            判断, 目前的items数组中, 是否已经包含了需要待加入的这个商品项. 如, items数组中已经有了 [时间简史], 那么只需要更改其数量,
+                并且, 更新为两本书的价格即可.
+         */
+        com.atguigu.pojo.CartItem findCartItem= this.items.get(cartItem.getId());
+        if (findCartItem != null) {
+            // 更新数量
+            findCartItem.setCount(findCartItem.getCount() + 1);
+            // 更新价格 -> 注意这里更新的方式
+            findCartItem.setTotalPrice(findCartItem.getPrice().multiply(new BigDecimal(findCartItem.getCount())));
+        } else {
+            this.items.put(cartItem.getId(), cartItem);
+        }
+    }
+
+    /**
+     * 删除项目
+     *
+     * @param id id
+     */
+    public void deleteItem(Integer id){
+        // 这个接口会自动判断, 如果没有就该方法的话, 就不删除.
+        this.items.remove(id);
+    }
+
+    /**
+     * 清晰
+     */
+    public void clear(){
+        this.items.clear();
+    }
+
+    /**
+     * 更新数
+     *
+     * @param id    id
+     * @param count 数
+     */
+    public void updateCount(Integer id, Integer count){
+        /*
+            先查看items数组中是否有改商品, 如果有, 则更新; 如果没有, 则不进行操作
+         */
+        CartItem findCartItem = this.items.get(id);
+        if (findCartItem != null) {
+            // 更新数量
+            findCartItem.setCount(count);
+            // 同时还需要更新该项书本的分总价格
+            findCartItem.setTotalPrice(findCartItem.getPrice().multiply(new BigDecimal(findCartItem.getCount())));
+        }
+
+    }
+
+
+
+
+
+
 }

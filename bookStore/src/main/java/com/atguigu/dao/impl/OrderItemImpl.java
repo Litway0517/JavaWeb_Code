4fe0014947772c58b2eadd54215bd9fcde980c6b
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-16 22:24:41 +0800

    bookStore 更新OrderItemImpl类 自增的字段实际上不用赋值

--- bookStore/src/main/java/com/atguigu/dao/impl/OrderItemImpl.java
+++ bookStore/src/main/java/com/atguigu/dao/impl/OrderItemImpl.java
@@ -20,7 +20,7 @@ public class OrderItemImpl extends BaseDao implements OrderItemDao {
      */
     @Override
     public int saveOrderItem(OrderItem orderItem) {
-        String sql = "INSERT INTO t_order_item(`id`, `name`, `count`, `price`, `total_price`, `order_id`) VALUES(?,?,?,?,?,?)";
-        return updateForOne(sql, orderItem.getOrderId(), orderItem.getName(), orderItem.getCount(), orderItem.getPrice(), orderItem.getTotalPrice(), orderItem.getOrderId());
+        String sql = "INSERT INTO t_order_item(`name`, `count`, `price`, `total_price`, `order_id`) VALUES(?,?,?,?,?)";
+        return updateForOne(sql, orderItem.getName(), orderItem.getCount(), orderItem.getPrice(), orderItem.getTotalPrice(), orderItem.getOrderId());
     }
 }

7ae4d20984c4eae2db89979d21584d6d60d35ce0
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-17 15:17:13 +0800

    bookStore OrderServlet程序中增加createOrder方法

--- bookStore/src/main/java/com/atguigu/web/servlet/order/OrderServlet.java
+++ bookStore/src/main/java/com/atguigu/web/servlet/order/OrderServlet.java
@@ -1,5 +1,8 @@
 package com.atguigu.web.servlet.order;
 
+import com.atguigu.pojo.Cart;
+import com.atguigu.pojo.User;
+import com.atguigu.service.impl.OrderServiceImpl;
 import com.atguigu.web.servlet.base.BaseServlet;
 
 import javax.servlet.ServletException;
@@ -9,6 +12,8 @@ import java.io.IOException;
 
 public class OrderServlet extends BaseServlet {
 
+    private OrderServiceImpl orderServiceImpl = new OrderServiceImpl();
+
     /**
      * 处理前端的结账功能请求
      * @param req 请求报文
@@ -17,7 +22,36 @@ public class OrderServlet extends BaseServlet {
      * @throws IOException IO异常
      */
     protected void createOrder(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
 
-        super.doGet(req, resp);
+        // 获取购物车cart对象
+        Cart cart = (Cart) req.getSession().getAttribute("cart");
+
+        // 获取userId
+        User loginUser = (User) req.getSession().getAttribute("user");
+        if (loginUser == null) {
+            req.getRequestDispatcher("pages/user/login.jsp").forward(req, resp);
+            /*
+                请求转发之后, 一般就不再做任何操作了, 所以这里:
+                    - 如果发现了用户没有登陆, 那么就直接转发到登陆界面, 让用户操作. 重点:　一定要有一个return语句.
+                      这样下面的代码就不会执行了
+
+                    －　如果用户已经登陆, 那么这个if语句体不执行, 自然就会向下执行.
+             */
+            return;
+        }
+        Integer userId = loginUser.getId();
+
+        // 调用orderService.createOrder()
+        String orderId = orderServiceImpl.createOrder(cart, userId);
+
+        // 保存订单编号到request域中
+        req.setAttribute("orderId", orderId);
+
+        // 请求转发:
+        req.getRequestDispatcher("pages/cart/checkout.jsp").forward(req, resp);
+
+
+
     }
 }

6a319f76af275408011f16d5b5dad7d6424dd9e0
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-17 16:11:39 +0800

    bookStore 解决购物车表单重新提交的bug F5的原因: 使用重定向

--- bookStore/src/main/java/com/atguigu/web/servlet/order/OrderServlet.java
+++ bookStore/src/main/java/com/atguigu/web/servlet/order/OrderServlet.java
@@ -45,12 +45,19 @@ public class OrderServlet extends BaseServlet {
         // 调用orderService.createOrder()
         String orderId = orderServiceImpl.createOrder(cart, userId);
 
-        // 保存订单编号到request域中
-        req.setAttribute("orderId", orderId);
+        // 保存订单编号到request域中. -> 更新为session域
+        // req.setAttribute("orderId", orderId);
+        req.getSession().setAttribute("orderId", orderId);
 
-        // 请求转发:
-        req.getRequestDispatcher("pages/cart/checkout.jsp").forward(req, resp);
+        // 请求转发
+        req.getRequestDispatcher("/pages/cart/checkout.jsp").forward(req, resp);
 
+
+        /*
+            上面的请求转发, 如果用户在 订单号 界面, 按住F5不断刷新的话, 那么就会一直重复提交购物车界面的表单, 这样数据库就会重复插入
+            使用重定向解决, 但是使用重定向, request域的数据就不能够带回到checkout.jsp界面, 因此要将orderId存储在session域中
+         */
+        resp.sendRedirect(req.getContentType() + "/pages/cart/checkout.jsp");
 
 
     }

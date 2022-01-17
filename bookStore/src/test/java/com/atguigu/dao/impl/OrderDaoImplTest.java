8b98dada33de13ae40e5084a84be354e71badc3c
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-17 18:14:44 +0800

    bookStore 方法测试: 查询所有订单

--- bookStore/src/test/java/com/atguigu/dao/impl/OrderDaoImplTest.java
+++ bookStore/src/test/java/com/atguigu/dao/impl/OrderDaoImplTest.java
@@ -5,13 +5,25 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 public class OrderDaoImplTest {
 
+    public OrderDaoImpl order = new OrderDaoImpl();
+
     @Test
     public void saveOrder() {
-        OrderDaoImpl order = new OrderDaoImpl();
         order.saveOrder(new Order("test001", new Date(), new BigDecimal("23.3"), 0, 11));
-
     }
+
+
+    @Test
+    public void queryAllOrders() {
+
+        List<Order> orders = order.queryAllOrders();
+        System.out.println(orders);
+    }
+
+
 }
+

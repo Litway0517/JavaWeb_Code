a619a7d7022fbcc590a413c162877758bac46adc
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-17 10:34:47 +0800

    bookStore 创建订单功能的测试 测试createOrder方法

---
+++ bookStore/src/test/java/com/atguigu/service/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package com.atguigu.service;
+
+import com.atguigu.pojo.Cart;
+import com.atguigu.pojo.CartItem;
+import com.atguigu.service.impl.OrderServiceImpl;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class OrderServiceTest {
+
+    @Test
+    public void createOrder() {
+
+        // 测试时, 先创建一个购物车
+        Cart cart = new Cart();
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 2, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(20, "计算机网络", 1, new BigDecimal(100), new BigDecimal(100)));
+
+        // 创建一个订单实现类OrderServiceImpl
+        OrderServiceImpl orderService = new OrderServiceImpl();
+        String order = orderService.createOrder(cart, 1);
+        System.out.println("订单号是 : " + order);
+
+    }
+}

b73746a6fdbb0f2866d3eeec4c943b373f69ed03
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-17 18:04:59 +0800

    bookStore 实现管理员功能: 实现查询所有订单内容方法

--- bookStore/src/main/java/com/atguigu/dao/impl/OrderDaoImpl.java
+++ bookStore/src/main/java/com/atguigu/dao/impl/OrderDaoImpl.java
@@ -4,6 +4,8 @@ import com.atguigu.dao.BaseDao;
 import com.atguigu.dao.OrderDao;
 import com.atguigu.pojo.Order;
 
+import java.util.List;
+
 /**
  * 订单DAOImpl
  *
@@ -23,5 +25,18 @@ public class OrderDaoImpl extends BaseDao implements OrderDao {
     public int saveOrder(Order order) {
         String sql = "INSERT INTO t_order(`order_id`, `create_time`, `price`, `status`, `user_id`) VALUES(?,?,?,?,?)";
         return updateForOne(sql, order.getOrderId(), order.getCreateTime(), order.getPrice(), order.getStatus(), order.getUserId());
+    }
+
+
+    /**
+     * 查询所有订单
+     *
+     * @return 返回所有订单的数目
+     */
+    @Override
+    public List<Order> queryAllOrders() {
+        String sql = "SELECT `order_id` orderId,`create_time` createTime,`price` price,`status` `status`,`user_id` userId  FROM `t_order`";
+        List<Order> orders = queryForList(Order.class, sql);
+        return orders;
     }
 }

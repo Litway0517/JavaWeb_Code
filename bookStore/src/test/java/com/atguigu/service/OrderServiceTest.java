532feb6ab7bffd7eab54392bce555a40a13adbb9
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-17 18:14:27 +0800

    bookStore 方法测试: 查询所有订单

--- bookStore/src/test/java/com/atguigu/service/OrderServiceTest.java
+++ bookStore/src/test/java/com/atguigu/service/OrderServiceTest.java
@@ -2,10 +2,12 @@ package com.atguigu.service;
 
 import com.atguigu.pojo.Cart;
 import com.atguigu.pojo.CartItem;
+import com.atguigu.pojo.Order;
 import com.atguigu.service.impl.OrderServiceImpl;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -26,4 +28,24 @@ public class OrderServiceTest {
         System.out.println("订单号是 : " + order);
 
     }
+
+    @Test
+    public void queryAllOrders() {
+
+        // 测试时, 先创建一个购物车
+        Cart cart = new Cart();
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 2, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(10, "黑客帝国 矩阵", 1, new BigDecimal(30), new BigDecimal(30)));
+        cart.addItem(new CartItem(20, "计算机网络", 1, new BigDecimal(100), new BigDecimal(100)));
+
+        // 创建一个订单实现类OrderServiceImpl
+        OrderServiceImpl orderServiceImpl = new OrderServiceImpl();
+        String order = orderServiceImpl.createOrder(cart, 1);
+        System.out.println("订单号是 : " + order);
+
+        List<Order> orders = orderServiceImpl.queryAllOrders();
+        System.out.println(orders);
+    }
+
+
 }

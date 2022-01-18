52ea4ac5832d95d24ea876fb88b346f76686105e
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-18 11:04:05 +0800

    bookStore 测试方法: 根据订单id查询订单的详细信息

--- bookStore/src/test/java/com/atguigu/dao/impl/OrderItemDaoImplTest.java
+++ bookStore/src/test/java/com/atguigu/dao/impl/OrderItemDaoImplTest.java
@@ -1,9 +1,11 @@
 package com.atguigu.dao.impl;
 
+import com.atguigu.pojo.Order;
 import com.atguigu.pojo.OrderItem;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class OrderItemDaoImplTest {
 
@@ -13,4 +15,19 @@ public class OrderItemDaoImplTest {
         orderItem.saveOrderItem(new OrderItem(null, "大话西游", 1, new BigDecimal("59.9"), new BigDecimal("59.9"), "test001"));
 
     }
+
+    @Test
+    public void queryOrderItemsByOrderId() {
+        OrderItemDaoImpl orderItemDaoImpl = new OrderItemDaoImpl();
+
+        OrderDaoImpl orderDaoImpl = new OrderDaoImpl();
+        List<Order> orders = orderDaoImpl.queryAllOrders();
+
+
+        List<OrderItem> orderItems = orderItemDaoImpl.queryOrderItemsByOrderId(orders.get(orders.size() - 1).getOrderId());
+        System.out.println(orderItems);
+
+
+    }
+
 }

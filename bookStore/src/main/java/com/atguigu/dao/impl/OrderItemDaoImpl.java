32b77e12b470d08ae095c55204b19330b9248d2e
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-18 11:03:39 +0800

    bookStore 实现接口方法: 根据订单id查询订单的详细信息

--- bookStore/src/main/java/com/atguigu/dao/impl/OrderItemDaoImpl.java
+++ bookStore/src/main/java/com/atguigu/dao/impl/OrderItemDaoImpl.java
@@ -4,6 +4,8 @@ import com.atguigu.dao.BaseDao;
 import com.atguigu.dao.OrderItemDao;
 import com.atguigu.pojo.OrderItem;
 
+import java.util.List;
+
 /**
  * 订单中的明细分类DAOImpl
  *
@@ -22,5 +24,18 @@ public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
     public int saveOrderItem(OrderItem orderItem) {
         String sql = "INSERT INTO t_order_item(`name`, `count`, `price`, `total_price`, `order_id`) VALUES(?,?,?,?,?)";
         return updateForOne(sql, orderItem.getName(), orderItem.getCount(), orderItem.getPrice(), orderItem.getTotalPrice(), orderItem.getOrderId());
+    }
+
+
+    /**
+     * 根据订单号查询订单的明细
+     * @param orderId 订单号
+     * @return 返回订单明细
+     */
+    @Override
+    public List<OrderItem> queryOrderItemsByOrderId(String orderId) {
+        String sql = "SELECT `id` `id`,`name` `name`,`count` `count`,`price` `price`,`total_price` `totalPrice`,`order_id` `orderId` FROM `t_order_item` WHERE `order_id` = ?";
+        List<OrderItem> orderItems = queryForList(OrderItem.class, sql, orderId);
+        return orderItems;
     }
 }

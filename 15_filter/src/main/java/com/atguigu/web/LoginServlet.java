6e591b21e5d82fff39b16418ab0e8ee3062600e3
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-20 11:47:04 +0800

    filter 登录测试界面

--- 15_filter/src/main/java/com/atguigu/web/LoginServlet.java
+++ 15_filter/src/main/java/com/atguigu/web/LoginServlet.java
@@ -11,7 +11,18 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
 
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            resp.getWriter().write("登录 成功!!!");
+            // 保存一下用户信息
+            req.getSession().setAttribute("user", username);
+        } else {
+            req.getRequestDispatcher("/pages/login.jsp").forward(req, resp);
+        }
 
     }
 }

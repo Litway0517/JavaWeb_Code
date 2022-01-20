57bc4dcae62a0e99bd9e1a2cb53b4791c6132eb6
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-20 19:06:28 +0800

    filter 两个Filter过滤器的执行顺序: 过滤器1的前置代码(调用chain.doFilter(req, resp)之前的代码都是前置代码) -> 过滤器2的前置代码 -> 目标资源 -> 过滤器2的后置代码 -> 过滤器1的后置代码

--- 15_filter/src/main/java/com/atguigu/filter/TestFilter1.java
+++ 15_filter/src/main/java/com/atguigu/filter/TestFilter1.java
@@ -3,24 +3,48 @@ package com.atguigu.filter;
 import javax.servlet.*;
 import java.io.IOException;
 
+/**
+ * 测试filter1
+ *
+ * @author DELL_
+ * @date 2022/01/20
+ */
 public class TestFilter1 implements Filter {
 
 
+    /**
+     * 初始化
+     *
+     * @param filterConfig 过滤器配置
+     * @throws ServletException servlet异常
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
+    /**
+     * 做过滤器
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param chain    链
+     * @throws IOException      IO异常
+     * @throws ServletException servlet异常
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         System.out.println("TestFilter1 前置代码");
 
-        doFilter(request, response, chain);
+        chain.doFilter(request, response);
 
         System.out.println("TestFilter1 后置代码");
     }
 
+    /**
+     * 摧毁
+     */
     @Override
     public void destroy() {
 

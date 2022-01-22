96777914c03daa135b05e76e00bc4e7d87721acc
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-22 16:40:34 +0800

    JSON_AJAX_i18n JSON字符串转换为Map对象时的类型类

---
+++ 16_json_ajax_i18n/src/main/java/com/atguigu/json/PersonMapType.java
@@ -0,0 +1,9 @@
+package com.atguigu.json;
+
+import com.atguigu.pojo.Person;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.Map;
+
+public class PersonMapType extends TypeToken<Map<Integer, Person>> {
+}

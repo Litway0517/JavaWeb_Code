39af8be197d0198fd1dea3ac8e9aadc2e00f854d
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-22 19:41:42 +0800

    JSON_AJAX_i18n 用来接收AJAX请求的servlet

---
+++ 16_json_ajax_i18n/src/main/java/com/atguigu/servlet/AjaxServlet.java
@@ -0,0 +1,22 @@
+package com.atguigu.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AjaxServlet extends BaseServlet {
+
+
+    /**
+     * 接受前端的AJAX请求
+     * ajax是指 Asynchronous Javascript And XML(异步JavaScript和XML)
+     *
+     * @param req  请求报文
+     * @param resp 响应报文
+     */
+    protected void javaScriptAjax(HttpServletRequest req, HttpServletResponse resp) {
+
+
+    }
+
+
+}

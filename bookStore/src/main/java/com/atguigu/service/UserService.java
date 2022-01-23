94ac4e44a660a8ca67e8e4578aeda4d537af6740
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-23 19:02:42 +0800

    JSON_AJAX_i18n

--- bookStore/src/main/java/com/atguigu/service/UserService.java
+++ bookStore/src/main/java/com/atguigu/service/UserService.java
@@ -14,6 +14,7 @@ public interface UserService {
 
     /**
      * 注册用户
+     *
      * @param user JavaBean对象 -> User类型
      */
     public void registerUser(User user);
@@ -21,6 +22,7 @@ public interface UserService {
 
     /**
      * 用户登录
+     *
      * @param user 前端用户登录传递过来的JavaBean对象
      * @return user对象
      */
@@ -29,9 +31,10 @@ public interface UserService {
 
     /**
      * 校验表中是否已经存在该 <用户名>
+     *
      * @param username 用户名
      * @return 若false, 则表中不存在该用户, 可注册.
-     *         若true, 则表中存在该用户 -> 不能再使用该用户名注册.
+     * 若true, 则表中存在该用户 -> 不能再使用该用户名注册.
      */
     public boolean existsUsername(String username);
 

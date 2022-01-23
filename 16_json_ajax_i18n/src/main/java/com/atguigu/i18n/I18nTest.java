4bb4d01b9a9972473d87b1ba4cd471e2a6b33518
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-23 23:02:55 +0800

    JSON_AJAX_i18n ResourceBundle

--- 16_json_ajax_i18n/src/main/java/com/atguigu/i18n/I18nTest.java
+++ 16_json_ajax_i18n/src/main/java/com/atguigu/i18n/I18nTest.java
@@ -3,6 +3,7 @@ package com.atguigu.i18n;
 import org.junit.Test;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class I18nTest {
 
@@ -17,6 +18,18 @@ public class I18nTest {
         // 获取中文, 中文的常量Locale对象
         System.out.println(Locale.CHINA);
         System.out.println(Locale.US);
+
+
+    }
+
+
+    @Test
+    public void testI18n() {
+        Locale us = Locale.US;
+
+        ResourceBundle i18n = ResourceBundle.getBundle("i18n", us);
+        System.out.println("username: " + i18n.getString("username"));
+        System.out.println("password: " + i18n.getString("password"));
 
 
     }

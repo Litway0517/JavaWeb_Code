d3d2185638eed44e44345615869ec860cc9a0f3d
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-23 11:47:58 +0800

    JSON_AJAX_i18n 增加两个测试方法 jQueryGet和jQueryPost 用来接收前端的jQuery封装好的get和post请求

--- 16_json_ajax_i18n/src/main/java/com/atguigu/servlet/AjaxServlet.java
+++ 16_json_ajax_i18n/src/main/java/com/atguigu/servlet/AjaxServlet.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 /**
  * ajax servlet
+ * ajax servlet
  *
  * @author DELL_
  * @date 2022/01/22
@@ -45,6 +46,7 @@ public class AjaxServlet extends BaseServlet {
 
 
     /**
+     * j ajax查询
      * jQuery库封装的Ajax请求
      *
      * @param req  要求事情
@@ -52,7 +54,7 @@ public class AjaxServlet extends BaseServlet {
      * @throws IOException IO异常
      */
     protected void jQueryAjax(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        System.out.println("jQueryAjax被调用了");
+        System.out.println("jQueryAjax   被调用了");
 
         Person person = new Person(1, "test");
 
@@ -60,7 +62,44 @@ public class AjaxServlet extends BaseServlet {
         Gson gson = new Gson();
         String personToJsonString = gson.toJson(person);
         resp.getWriter().write(personToJsonString);
+    }
 
+
+    /**
+     * jQueryGet
+     *
+     * @param req  要求事情
+     * @param resp 分别地
+     * @throws IOException IO异常
+     */
+    protected void jQueryGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("jQueryGet   被调用了");
+
+        Person person = new Person(2, "jQueryGet");
+
+        // 将person对象转换成JSON字符串格式
+        Gson gson = new Gson();
+        String personToJsonString = gson.toJson(person);
+        resp.getWriter().write(personToJsonString);
+
+    }
+
+    /**
+     * jQueryPost
+     *
+     * @param req  要求事情
+     * @param resp 分别地
+     * @throws IOException IO异常
+     */
+    protected void jQueryPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("jQueryPost   被调用了");
+
+        Person person = new Person(3, "jQueryPost");
+
+        // 将person对象转换成JSON字符串格式
+        Gson gson = new Gson();
+        String personToJsonString = gson.toJson(person);
+        resp.getWriter().write(personToJsonString);
     }
 
 

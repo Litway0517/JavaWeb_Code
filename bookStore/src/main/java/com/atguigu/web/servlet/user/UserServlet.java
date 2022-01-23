ec506acb4cfde66c332b42715ecbb283940486ef
Mingw0717-Dell <xmw0517@gmail.com>
2022-01-23 19:02:32 +0800

    JSON_AJAX_i18n 使用AJAX请求优化前端的注册界面 当用户输入用户名的时候就及时反馈该用户名是否可用

--- bookStore/src/main/java/com/atguigu/web/servlet/user/UserServlet.java
+++ bookStore/src/main/java/com/atguigu/web/servlet/user/UserServlet.java
@@ -5,12 +5,14 @@ import com.atguigu.service.UserService;
 import com.atguigu.service.impl.UserServiceImpl;
 import com.atguigu.utils.WebUtils;
 import com.atguigu.web.servlet.base.BaseServlet;
+import com.google.gson.Gson;
 
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 import static com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
 
@@ -21,23 +23,16 @@ import static com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
  * @date 2021/11/14
  */
 public class UserServlet extends BaseServlet {
-    /**
-     * 一个很基本的思想 ->
-     *      所有前端发送过来的数据, 到了后端都需要将其封装成为一个Bean对象.
-     *      或者直接理解为, Bean才是编程时, 要着重注意的一个点, 将需要的数据都考虑为一个Bean
-     *      每一次操作的基本单位是Bean
-     *
-     *      一句话, 前后端交互的最基础的数据就是一个Bean, 通过JSON文件格式进行交换
-     */
 
-    UserService userService = new UserServiceImpl();
+    UserService userServiceImpl = new UserServiceImpl();
 
     /**
      * 处理前端登录请求
-     * @param req 请求报文
+     *
+     * @param req  请求报文
      * @param resp 响应报文
      * @throws ServletException servlet异常
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      */
     public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -47,7 +42,7 @@ public class UserServlet extends BaseServlet {
 
 
         // 检查用户输入的相关信息是否正确
-        User loginUser = userService.login(user);
+        User loginUser = userServiceImpl.login(user);
         if (loginUser == null) {
             /*
                 控制台提示一下 -> 信息错误, 请重新登录
@@ -73,7 +68,8 @@ public class UserServlet extends BaseServlet {
 
     /**
      * 登出用户
-     * @param req 请求报文
+     *
+     * @param req  请求报文
      * @param resp 响应报文
      * @throws IOException IO异常
      */
@@ -90,10 +86,11 @@ public class UserServlet extends BaseServlet {
 
     /**
      * 处理前端注册请求
-     * @param req 请求报文
+     *
+     * @param req  请求报文
      * @param resp 响应报文
      * @throws ServletException servlet异常
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      */
     protected void regist(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -115,7 +112,7 @@ public class UserServlet extends BaseServlet {
         // 目前还没有服务器生成验证码 -> 暂时将验证码写成固定的abcde, 且不区分大小写
         if (token != null && code.equalsIgnoreCase(token)) {
             // 检查用户名是否可用 -> Web不能直接调用DAO层, 而是使用Service层完成的, 因此需要在 '方法体外' 实例化UserService层
-            if (userService.existsUsername(user.getUsername())) {
+            if (userServiceImpl.existsUsername(user.getUsername())) {
                 // 返回true表示不能使用该用户名注册 -> 提醒 + 跳转到注册界面
                 System.out.println("用户名 [" + user.getUsername() + "] 已存在! ");
 
@@ -129,7 +126,7 @@ public class UserServlet extends BaseServlet {
             }
             else {
                 // 用户名不存在表中 -> 可用情况 -> 调用UserService表存到表中 -> 在跳转到注册成功界面
-                userService.registerUser(user);
+                userServiceImpl.registerUser(user);
                 req.getRequestDispatcher("/pages/user/regist_success.jsp").forward(req, resp);
             }
         }
@@ -144,6 +141,34 @@ public class UserServlet extends BaseServlet {
             // 这里面也验证了, getRequestDispatcher方法是具有跳转页面的功能的
             req.getRequestDispatcher("/pages/user/regist.jsp").forward(req, resp);
         }
+    }
+
+
+    /**
+     * 检查用户名存在
+     *
+     * @param req  要求事情
+     * @param resp 分别地
+     * @throws IOException IO异常
+     */
+    protected void checkUsernameExist(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+
+        // 获取用户名参数
+        String username = req.getParameter("username");
+
+        // 调用userService层检查用户名唯一性
+        boolean flag = userServiceImpl.existsUsername(username);
+
+        // 将结果封装成Map对象
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("existUsername", flag);
+
+        // 转换为json字符串传送
+        Gson gson = new Gson();
+        String json = gson.toJson(resultMap);
+        resp.getWriter().write(json);
+
     }
 
 
